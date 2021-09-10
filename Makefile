ec21fd7b513ebf377a97b23b676f8eba3290e7c3
Ken Lauer <klauer@slac.stanford.edu>
2021-09-10 09:23:30 -0700

    Initial commit

---
+++ Makefile
@@ -0,0 +1,19 @@
+ENV_NAME ?= ads-log-daemon-env
+PYTHON := $(ENV_NAME)/bin/python
+PIP := $(PYTHON) -m pip
+
+
+initialize:
+	if [ ! -d "$(ENV_NAME)" ]; then \
+		python -m venv $(ENV_NAME); \
+	fi
+
+	$(PIP) install --upgrade pip
+	$(PIP) install -r requirements.txt
+
+
+run:
+	bash ./run.sh
+
+
+.PHONY: initialize run
---
+++ run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ENV_NAME=ads-log-daemon-env
+PYTHON=${SCRIPT_DIR}/${ENV_NAME}/bin/python
+
+export LOG_DAEMON_HOST=172.21.32.90
+export LOG_DAEMON_NET_ID=172.21.32.90.1.1
+export LOG_DAEMON_ROUTE_NAME=ads-log-daemon
+
+# LOG_DAEMON_HOST_NAME=
+export LOG_DAEMON_LDAP_SEARCH_BASE=ou=Subnets,dc=reg,o=slac
+export LOG_DAEMON_LDAP_SERVER=ldap://psldap1.pcdsn
+export LOG_DAEMON_HOST_PREFIXES=plc-*,bhc-*
+export LOG_DAEMON_SEARCH_PERIOD=900
+
+export LOG_DAEMON_TARGET_HOST=ctl-logsrv01
+export LOG_DAEMON_TARGET_PORT=54321
+
+export LOG_DAEMON_TIMESTAMP_THRESHOLD=60
+export LOG_DAEMON_INFO_PERIOD=60
+
+export LOG_DAEMON_SOURCE_ENCODING=latin-1
+export LOG_DAEMON_ENCODING=utf-8
+
+set -x
+${PYTHON} -u ${ENV_NAME}/bin/ads-log-daemon --ldap
